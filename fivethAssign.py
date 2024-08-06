90b4b6b2e76018778d814c42e63462718c7f98fc
Sumaanyu Nayak <nayaksumaanyu@gmail.com>
2024-08-07 01:22:26 +0530

    linear and binary search done

--- fivethAssign.py
+++ fivethAssign.py
@@ -11,14 +11,14 @@ def linearSearch(lst,ele):
     return ret,count
 
 def binarySearch(lst,s,e,ele,count = 0):
-    if s==len(lst) or e==0:
-        return -1,-1
+    if s>e:
+        return (-1,count)
     else:
         s=s
         e=e
         m=int((s+e)/2)
         if lst[m]==ele:
-            return m,count
+            return (m,count)
         elif ele>lst[m]:
             return binarySearch(lst,m+1,e,ele,count+1)
         elif ele<lst[m]:
@@ -28,13 +28,14 @@ def binarySearch(lst,s,e,ele,count = 0):
 input_str_a = input("Enter elements the list: ")
 a = input_str_a.split()  
 a = sorted([int(num) for num in a])
-ele=int(input("Enter the element to binSearch(a,0,len(a),ele)search: "))
+ele=int(input("Enter the element to search: "))
 
 
 print()
 print("Using Binary Search")
-if binarySearch(a,0,len(a),ele)[1]<0:
+if binarySearch(a,0,len(a),ele)[0]<0:
     print("Element not found")
+    print("The no of comparisions:",binarySearch(a,0,len(a),ele)[1])
     
 else:
     print("Element found at", binarySearch(a,0,len(a),ele)[0])
@@ -45,6 +46,7 @@ print()
 print("Using linear search")
 if linearSearch(a,ele)[0]<0:
     print("Element not found")
+    print("The no of comparisions:",linearSearch(a,ele)[1])
 else:
     print(f"Element found at", linearSearch(a,ele)[0])
     print("The no of comparisions:",linearSearch(a,ele)[1])
